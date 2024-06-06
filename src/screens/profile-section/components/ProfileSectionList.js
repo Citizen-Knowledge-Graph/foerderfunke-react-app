6ee550f85c4bcd5089cf00b18390ba03eb672a30
wbglaeser <ben.glaeser@tuta.io>
2024-06-06 16:46:56 +0100

    allow user to return to previous question

--- src/screens/profile-section/components/ProfileSectionList.js
+++ src/screens/profile-section/components/ProfileSectionList.js
@@ -1,62 +1,50 @@
-import React from 'react';
-import {Card, CardContent, TextField, Typography} from '@mui/material';
+import React, {useState} from 'react';
 import VStack from "../../../components/VStack";
-import HStack from "../../../components/HStack";
-import {yellow} from "@mui/material/colors";
+import ProfileSectionField from "./ProfileSectionField";
 
 const ProfileSectionList = ({profileSectionData}) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleConfirm = () => {
+        if (currentIndex < profileSectionData.fields.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        } else {
+            // Handle the case when all sections are completed
+            console.log('All sections completed');
+        }
+    };
+
+    const handleBack = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        } else {
+            // Handle the case when all sections are completed
+            console.log('All sections completed');
+        }
+    };
+
+    const handleSkip = () => {
+        if (currentIndex < profileSectionData.fields.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        } else {
+            // Handle the case when all sections are completed
+            console.log('All sections completed');
+        }
+    };
+
+    const currentSection = profileSectionData.fields[currentIndex];
 
     return (
-        <VStack sx={{width: '100%'}}>
+        <VStack sx={{width: '100%', paddingTop: '50px'}}>
             <VStack gap={1}>
-                {
-                    profileSectionData.fields.map((section) => {
-                        return (
-                            <VStack gap={1}>
-                            <HStack>
-                                <Card sx={styles.infoCard} data-testid="card">
-                                    <CardContent sx={styles.infoCardContent} data-testid="card-content">
-                                        <Typography variant="body1" gutterBottom sx={styles.titleText}>
-                                            {section.title}
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </HStack>
-                                <TextField
-                                    label="Username"
-                                    variant="outlined"
-                                    fullWidth
-                                />
-                            </VStack>
-                        )
-                    })
-                }
+                <ProfileSectionField profileSectionField={currentSection}
+                                     currentIndex={currentIndex}
+                                     handleConfirm={handleConfirm}
+                                     handleBack={handleBack}
+                                     handleSkip={handleSkip}/>
             </VStack>
         </VStack>
     );
-};
-
-const styles = {
-    titleText: {
-        fontWeight: 'bold',
-        margin: '0px'
-    },
-    infoCard: {
-        width: '100%',
-        borderRadius: '15px',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: yellow[200],
-        boxShadow: 'none',
-    },
-    infoCardContent: {
-        padding: '16px',
-        "&:last-child": {
-            paddingBottom: '16px',
-        }
-    },
 };
 
 export default ProfileSectionList;
