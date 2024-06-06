44025217acf384ceb44920b559ba3169ae68793a
wbglaeser <ben.glaeser@tuta.io>
2024-06-06 16:08:33 +0100

    update base url to pick up env

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production' ?
+    '/foerderfunke-react-app/' :
+    '/';
+
 // Create an Axios instance
 const api = axios.create({
-    baseURL: '/', // Set the base URL
+    baseURL: baseURL
 });
 
 export default api;
