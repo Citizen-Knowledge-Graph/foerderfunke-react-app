1ae2b9c378ff0aca62e2492955ca45663169f7a8
wbglaeser <ben.glaeser@tuta.io>
2024-06-17 18:33:39 +0200

    update elibility item

--- src/screens/eligibilty-overview/components/EligibilityOverviewItem.js
+++ src/screens/eligibilty-overview/components/EligibilityOverviewItem.js
@@ -1,57 +1,41 @@
-import React, { useState } from 'react';
-import {Typography} from '@mui/material';
+import React, {useState} from 'react';
+import {IconButton, Typography} from '@mui/material';
 import VStack from "../../../components/VStack";
 import HStack from "../../../components/HStack";
 import {green, red, grey} from "@mui/material/colors";
-import { Circle, Add, Remove } from "@mui/icons-material";
+import {Circle, Add, Remove} from "@mui/icons-material";
 
-const EligibilityOverviewItem = ({items, eligble}) => {
-    const [visibleDescriptions, setVisibleDescriptions] = useState({});
+const EligibilityOverviewItem = ({item, eligible}) => {
+    const [showDescription, setShowDescription] = useState(false);
 
-    const toggleDescription = (index) => {
-        setVisibleDescriptions(prevState => ({
-            ...prevState,
-            [index]: !prevState[index]
-        }));
+    const toggleDescription = () => {
+        setShowDescription(prevState => !prevState);
     };
 
-    const color = (eligble === 'eligible') ? green[500] : ((eligble === 'non-eligible') ? red[500] :
+    const color = (eligible === 'eligible') ? green[500] : ((eligible === 'non-eligible') ? red[500] :
         grey[500]);
-    const headerText = (eligble === 'eligible') ? 'Berechtigt für:' : ((eligble === 'non-eligible') ? 'Nicht berechtigt für:' :
-        'Unklar für:');
-
 
     return (
-        <VStack sx={{width: '100%'}}>
-            <HStack justifyContent={'flex-start'} sx={{width: '100%'}}>
-                <Typography variant="h6" gutterBottom sx={styles.titleText}>
-                    {headerText}
-                </Typography>
+        <HStack justifyContent={'flex-start'} sx={{width: '100%'}}>
+            <Circle sx={{color: color}}/>
+            <HStack justifyContent={'space-between'} sx={{width: '100%'}}>
+                <VStack gap={0} alignItems={'flex-start'}>
+                    <Typography variant="body1" gutterBottom sx={styles.itemTitle}>
+                        {item.title}
+                    </Typography>
+                    {showDescription && (
+                        <Typography variant="body2" gutterBottom>
+                            {item.description}
+                        </Typography>
+                    )}
+                </VStack>
             </HStack>
-            <VStack gap={1} alignItems={'center'} sx={{width: '100%'}}>
-                {items.map((item, index) => (
-                    <HStack key={index} justifyContent={'flex-start'} sx={{width: '100%'}}>
-                        <Circle sx={{color: color}}/>
-                        <HStack justifyContent={'space-between'} sx={{width: '100%'}}>
-                            <VStack gap={0} alignItems={'flex-start'}>
-                                <Typography variant="body1" gutterBottom sx={styles.itemTitle}>
-                                    {item.title}
-                                </Typography>
-                                {
-                                    showDescription ? (
-                                        <Typography variant="body2" gutterBottom>
-                                            {item.description}
-                                        </Typography>
-                                    ) : null
-                                }
-                            </VStack>
-                        </HStack>
-                        <Add/>
-                    </HStack>
-                ))}
+            <VStack gap={0} alignItems={'flex-start'}>
+                <IconButton onClick={toggleDescription} size="small">
+                    {showDescription ? <Remove/> : <Add/>}
+                </IconButton>
             </VStack>
-        </VStack>
-
+        </HStack>
     );
 };
 
--- src/screens/eligibilty-overview/components/EligibilityOverviewList.js
+++ src/screens/eligibilty-overview/components/EligibilityOverviewList.js
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {Typography} from '@mui/material';
 import VStack from "../../../components/VStack";
 import HStack from "../../../components/HStack";
-import {green, red, grey} from "@mui/material/colors";
-import { Circle, Add, Remove } from "@mui/icons-material";
+import EligibilityOverviewItem from "./EligibilityOverviewItem";
 
-const EligibilityOverviewList = ({items, eligble}) => {
-    const [visibleDescriptions, setVisibleDescriptions] = useState({});
-
-    const toggleDescription = (index) => {
-        setVisibleDescriptions(prevState => ({
-            ...prevState,
-            [index]: !prevState[index]
-        }));
-    };
-
-    const color = (eligble === 'eligible') ? green[500] : ((eligble === 'non-eligible') ? red[500] :
-        grey[500]);
-    const headerText = (eligble === 'eligible') ? 'Berechtigt für:' : ((eligble === 'non-eligible') ? 'Nicht berechtigt für:' :
+const EligibilityOverviewList = ({items, eligible}) => {
+    const headerText = (eligible === 'eligible') ? 'Berechtigt für:' : ((eligible === 'non-eligible') ? 'Nicht berechtigt für:' :
         'Unklar für:');
-
 
     return (
         <VStack sx={{width: '100%'}}>
@@ -30,24 +17,7 @@ const EligibilityOverviewList = ({items, eligble}) => {
             </HStack>
             <VStack gap={1} alignItems={'center'} sx={{width: '100%'}}>
                 {items.map((item, index) => (
-                    <HStack key={index} justifyContent={'flex-start'} sx={{width: '100%'}}>
-                        <Circle sx={{color: color}}/>
-                        <HStack justifyContent={'space-between'} sx={{width: '100%'}}>
-                            <VStack gap={0} alignItems={'flex-start'}>
-                                <Typography variant="body1" gutterBottom sx={styles.itemTitle}>
-                                    {item.title}
-                                </Typography>
-                                {
-                                    showDescription ? (
-                                        <Typography variant="body2" gutterBottom>
-                                            {item.description}
-                                        </Typography>
-                                    ) : null
-                                }
-                            </VStack>
-                        </HStack>
-                        <Add/>
-                    </HStack>
+                    <EligibilityOverviewItem key={index} item={item} eligible={eligible}/>
                 ))}
             </VStack>
         </VStack>
