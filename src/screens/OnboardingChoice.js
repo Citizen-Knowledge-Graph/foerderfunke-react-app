2f34b04d823b64d73d27bf007c5f85e6e9639e5c
wbglaeser <ben.glaeser@tuta.io>
2024-06-30 22:52:27 +0200

    new choice cards

--- src/screens/OnboardingChoice.js
+++ src/screens/OnboardingChoice.js
@@ -1,83 +1,83 @@
 import React from 'react';
-import {Typography, Button, Box, Card, CardContent, CardActions} from '@mui/material';
+import {Typography} from '@mui/material';
 import {Link} from 'react-router-dom';
 import Layout from '../components/Layout';
+import VStack from "../components/VStack";
+import HStack from "../components/HStack";
 
 const OnboardingChoice = () => {
     return (
         <Layout>
-            <Box sx={styles.container}>
+            <VStack gap={1} sx={styles.container} justifyContent={'center'}>
                 <Typography variant="h3" gutterBottom sx={styles.titleText}>
-                    Zwei Wege
+                    Your choice
                 </Typography>
                 <Typography variant="body1" gutterBottom sx={styles.subTitleText}>
-                    Du hast zwei Möglichkeiten, Leistungen und Förderungen für dich zu entdecken
+                    Three ways of discovering benefits for you
                 </Typography>
-            </Box>
-            <Box sx={styles.cardContainer} data-testid="card-container">
-                <Card sx={styles.card} data-testid="card">
-                    <CardContent sx={styles.cardContent} data-testid="card-content">
-                        <Typography variant="h5" component="div">
-                            Entdecke personalisierte Leistungen
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" component={Link} to="/onboarding-welcome">
-                            Los geht's
-                        </Button>
-                    </CardActions>
-                </Card>
-                <Card sx={styles.card} data-testid="card">
-                    <CardContent sx={styles.cardContent} data-testid="card-content">
-                        <Typography variant="h5" component="div">
-                            Browse den gesamten Katalog
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" component={Link} to="/onboarding">
-                            Los geht's
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Box>
+            </VStack>
+            <VStack data-testid="card-container" sx={{width: "100%"}}>
+                <Link to="/onboarding-welcome" style={{textDecoration: 'none', color: "black", width: '100%'}}>
+                    <HStack justifyContent={'flex-start'} gap={1} sx={styles.card}>
+                        <VStack gap={0} justifyContent={'flex-end'}>
+                            <Typography sx={styles.cardContentTitle}>
+                                Quick eligibility check
+                            </Typography>
+                            <Typography sx={styles.cardContentSubTitle}>
+                                Based on 10 benefits
+                            </Typography>
+                        </VStack>
+                    </HStack>
+                </Link>
+            </VStack>
+            <VStack data-testid="card-container" sx={{width: "100%"}}>
+                <Link to="/onboarding-welcome" style={{textDecoration: 'none', color: "black", width: '100%'}}>
+                    <HStack justifyContent={'flex-start'} gap={1} sx={styles.card}>
+                        <VStack gap={1} justifyContent={'flex-end'}>
+                            <Typography variant="body" sx={styles.cardContentTitle}>
+                                Browse benefits
+                            </Typography>
+                            <Typography variant="h6" sx={styles.cardContentSubTitle}>
+                                Filter by topic and from A-Z
+                            </Typography>
+                        </VStack>
+                    </HStack>
+                </Link>
+            </VStack>
         </Layout>
     );
 };
 
 const styles = {
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center', // Center horizontally
-        textAlign: 'center', // Ensure text alignment is also centered
-    },
     titleText: {
-        fontWeight: 'bold', // Make text bold
+        fontWeight: 'bold',
+        textAlign: 'center'
     },
     subTitleText: {
-        fontSize: '20px',
-        fontWeight: '400'
-    },
-    cardContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        width: '100%',
-        gap: '20px',
+        fontSize: '28px',
+        fontWeight: '400',
+        textAlign: 'center'
     },
     card: {
-        width: '100%',
-        height: '200px',
-        borderRadius: '20px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        backgroundColor: '#f0f0f0',
-        boxShadow: 'none'
+        height: '175px',
+        padding: '16px',
+        borderRadius: '15px',
+        backgroundImage: `url('foerderfunke-react-app/assets/images/quick-check.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
     },
-    cardContent: {
-        flexGrow: 1,
+    cardContentTitle: {
+        fontSize: '36px',
+        fontWeight: 'bold',
+        color: "white"
     },
+    cardContentSubTitle: {
+        fontSize: '16px',
+        fontWeight: '400',
+        color: "white"
+    }
 };
 
 export default OnboardingChoice;
