25c4f4b972c0d41c21a2b2bca06abd9f786ffd34
wbglaeser <ben.glaeser@tuta.io>
2024-07-07 20:47:07 +0200

    add image list

--- src/screens/landing-page/LandingPage.js
+++ src/screens/landing-page/LandingPage.js
@@ -2,6 +2,7 @@ import VStack from "../../components/VStack";
 import {Card, CardContent, Typography} from "@mui/material";
 import Layout from "../../components/Layout";
 import HStack from "../../components/HStack";
+import LandingPageImageList from "./components/LandingPageImageList";
 import {yellow} from "@mui/material/colors";
 import {Link} from "react-router-dom";
 import React from "react";
@@ -9,22 +10,32 @@ import React from "react";
 const LandingPage = () => {
     return (
         <Layout>
-            <VStack sx={{width: "100%"}}>
-                <HStack justifyContent={'flex-start'}>
-                    <Typography sx={styles.headerSectionTitle}>Check personalised benefits and financial support</Typography>
-                </HStack>
-                <HStack>
-                    <Card sx={styles.headerSectionButtonCard}>
-                        <Link to={'/onboarding-choice'} style={{textDecoration: 'none', color: "black", width: '100%'}}>
-                            <CardContent sx={styles.headerSectionButtonCardContent}>
-                                <HStack gap={1} alignItems={'center'}>
-                                    <Typography sx={styles.headerSectionButtonCardText}>
-                                        Try FörderFunke
-                                    </Typography>
-                                </HStack>
-                            </CardContent>
-                        </Link>
-                    </Card>                </HStack>
+            <VStack sx={{width: "100%"}} gap={3}>
+                <VStack sx={{width: "100%"}}>
+                    <HStack justifyContent={'flex-start'}>
+                        <Typography sx={styles.headerSectionTitle}>Check personalised benefits and financial
+                            support</Typography>
+                    </HStack>
+                    <HStack>
+                        <Card sx={styles.headerSectionButtonCard}>
+                            <Link to={'/onboarding-choice'}
+                                  style={{textDecoration: 'none', color: "black", width: '100%'}}>
+                                <CardContent sx={styles.headerSectionButtonCardContent}>
+                                    <HStack gap={1} alignItems={'center'}>
+                                        <Typography sx={styles.headerSectionButtonCardText}>
+                                            Try FörderFunke
+                                        </Typography>
+                                    </HStack>
+                                </CardContent>
+                            </Link>
+                        </Card>
+                    </HStack>
+                </VStack>
+                <VStack sx={{width: "100%"}}>
+                    <HStack>
+                        <LandingPageImageList/>
+                    </HStack>
+                </VStack>
             </VStack>
         </Layout>
     );
---
+++ src/screens/landing-page/components/LandingPageImageList.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import {ImageListItemBar} from "@mui/material";
+
+export default function LandingPageImageList() {
+    return (
+        <ImageList sx={{width: 500, height: 450}} cols={3} rowHeight={164}>
+            {itemData.map((item) => (
+                <ImageListItem key={item.img}>
+                    <img
+                        srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                        src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                        alt={item.title}
+                        loading="lazy"
+                        style={{borderRadius: '5px'}}
+                    />
+                    <ImageListItemBar
+                        title={'Kindergeld'}
+                        sx={{
+                            '& .MuiImageListItemBar-title': {
+                                fontSize: '12px',
+                            },
+                        }}
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
+}
+
+const itemData = [
+    {
+        img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+        title: 'Breakfast',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+        title: 'Burger',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+        title: 'Camera',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+        title: 'Coffee',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+        title: 'Hats',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+        title: 'Honey',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+        title: 'Basketball',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+        title: 'Fern',
+    },
+    {
+        img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+        title: 'Mushrooms',
+    }
+];
