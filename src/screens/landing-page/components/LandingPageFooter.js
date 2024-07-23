88ba18ca50623e4f10a0674b39d8fc1e5376af31
wbglaeser <ben.glaeser@tuta.io>
2024-07-23 19:26:18 +0200

    apply styles to footer bar

--- src/screens/landing-page/components/LandingPageFooter.js
+++ src/screens/landing-page/components/LandingPageFooter.js
@@ -1,7 +1,13 @@
 import VStack from "../../../components/VStack";
 import HStack from "../../../components/HStack";
-import {Typography} from "@mui/material";
+import {Card, CardContent, Typography} from "@mui/material";
 import globalStyles from "../../../styles/styles";
+import Divider from '@mui/material/Divider';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import {Link} from "react-router-dom";
+import React from "react";
 
 const LandingPageFooter = () => {
     return (
@@ -14,17 +20,20 @@ const LandingPageFooter = () => {
                 </HStack>
             </VStack>
             <VStack alignItems={'center'} sx={{width: "100%"}}>
-                <HStack>
-                    <Typography>
+                <HStack gap={1}>
+                    <EmailIcon />
+                    <Typography sx={styles.contactText}>
                         info@foerderfunke.org
                     </Typography>
                 </HStack>
-                <HStack>
-                    <Typography>
+                <HStack gap={1}>
+                    <LinkedInIcon/>
+                    <Typography sx={styles.contactText}>
                         LinkedIn FoerderFunke
                     </Typography>
                 </HStack>
             </VStack>
+            <Divider variant="middle"/>
             <VStack alignItems={'center'} sx={{width: "100%"}}>
                 <HStack>
                     <Typography>
@@ -46,10 +55,25 @@ const LandingPageFooter = () => {
                         About us
                     </Typography>
                 </HStack>
-                <HStack>
-                    <Typography>
-                        Copyright
+                <HStack alignItems={'center'} gap={1}>
+                    <CopyrightIcon/>
+                    <Typography sx={styles.copyrightText}>
+                        2024 by FörderFunke. All rights reserved.
                     </Typography>
+                </HStack>
+                <HStack justifyContent={'center'} sx={{width: '100%'}}>
+                    <Card sx={styles.headerSectionButtonCard}>
+                        <Link to={'/onboarding-choice'}
+                              style={{textDecoration: 'none', color: "black", width: '100%'}}>
+                            <CardContent sx={styles.headerSectionButtonCardContent}>
+                                <HStack gap={1} alignItems={'center'}>
+                                    <Typography sx={styles.headerSectionButtonCardText}>
+                                        Try FörderFunke
+                                    </Typography>
+                                </HStack>
+                            </CardContent>
+                        </Link>
+                    </Card>
                 </HStack>
             </VStack>
         </VStack>
@@ -65,7 +89,35 @@ const styles = {
     footerHeaderTitle: {
         fontWeight: "bold",
         fontSize: "28px"
-    }
+    },
+    contactText: {
+        textDecoration: "underline",
+    },
+    copyrightText: {
+        fontSize: "14px",
+    },
+    headerSectionButtonCard: {
+        backgroundColor: 'white',
+        boxShadow:
+            'none',
+        borderRadius:
+            '12px',
+    },
+    headerSectionButtonCardContent: {
+        padding: "3px 6px 4px 8px",
+        "&:last-child":
+            {
+                paddingBottom: '3px',
+            }
+        ,
+    },
+    headerSectionButtonCardText: {
+        color: "black",
+        fontSize:
+            '20px',
+        fontWeight:
+            'bold',
+    },
 }
 
 export default LandingPageFooter;
