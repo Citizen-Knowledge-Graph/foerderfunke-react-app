5ede1a73385c350e44e7a335605b92fff9f9ae02
wbglaeser <ben.glaeser@tuta.io>
2024-09-01 21:01:00 +0200

    fix padding issue on completed screen

--- src/screens/profile-section/components/ProfileSectionCompleted.js
+++ src/screens/profile-section/components/ProfileSectionCompleted.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import VStack from "../../../components/VStack";
 import HStack from "../../../components/HStack";
-import {Typography} from "@mui/material";
+import { Typography } from "@mui/material";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import ButtonCard from "../../../components/ButtonCard";
 import globalStyles from "../../../styles/styles";
-import {useMetadataStore} from "../../../storage/zustand";
-import {UserModel} from "../../../models/UserModel";
+import { useMetadataStore } from "../../../storage/zustand";
+import { UserModel } from "../../../models/UserModel";
 import dayjs from "dayjs";
 
 const ProfileSectionCompleted = () => {
     const metadata = useMetadataStore((state) => state.metadata);
     const userProfile = UserModel.retrieveUserData("ff:quick-check-user");
 
-    // the next 3 methods are the same as in BenefitPageRules, put them into utils? TODO
-
     const expand = (uri) => {
         return uri.startsWith("ff:") ? "https://foerderfunke.org/default#" + uri.split(":")[1] : uri;
-    }
+    };
 
     const getChoiceLabel = (value, dfObj) => {
         if (value === "true") return "yes";
         if (value === "false") return "no";
         return dfObj.choices.find(choice => expand(choice.value) === expand(value)).label;
-    }
+    };
 
     const convertUserValueRaw = (raw, dfObj) => {
         if (Array.isArray(raw)) {
@@ -39,52 +37,56 @@ const ProfileSectionCompleted = () => {
             return dayjs(raw).format("YYYY-MM-DD");
         }
         return raw;
-    }
+    };
 
     const buildUserProfile = () => {
-        const elements = [];
-        for (const [key, value] of Object.entries(userProfile)) {
-            console.log("***** new key value *****")
-            console.log("key: ", key)
-            console.log("value: ", value)
-            if (key.startsWith("@")) continue;
-            let dfObj = metadata.df[expand(key)];
-            let dfLabel = dfObj.label;
-            let dfValue = convertUserValueRaw(value, dfObj);
-            elements.push(
-                <span key={key}>
-                    <span style={{fontWeight: "bold"}} key={key}>{dfLabel}:</span>
-                    <span> {dfValue}</span>
-                </span>
-            );
-        }
-        return elements;
-    }
+        return (
+            <VStack sx={{ width: '100%'}}>
+                {Object.entries(userProfile).map(([key, value]) => {
+                    if (key.startsWith("@")) return null;
+                    const dfObj = metadata.df[expand(key)];
+                    const dfLabel = dfObj.label;
+                    const dfValue = convertUserValueRaw(value, dfObj);
+                    return (
+                        <Typography key={key} variant="body1" sx={{ marginBottom: '10px' }}>
+                            <strong>{dfLabel}:</strong> {dfValue}
+                        </Typography>
+                    );
+                })}
+            </VStack>
+        );
+    };
 
     return (
-        <VStack sx={{height: "90vh"}}>
-            <VStack sx={{height: "5vh"}}>
-            </VStack >
-            <VStack gap={5} justifyContent={'flex-start'} sx={{height: "85vh"}}>
-                <HStack justifyContent={'center'} sx={{width: "100%"}}>
+        <VStack>
+            <VStack />
+            <VStack gap={5} justifyContent={'flex-start'}>
+                <HStack justifyContent={'center'} sx={{ width: "100%" }}>
                     <VStack gap={1} alignItems={'center'} sx={styles.completeTextBox}>
-                        <CheckCircleIcon sx={styles.icon}/>
+                        <CheckCircleIcon sx={styles.icon} />
                     </VStack>
                 </HStack>
-                <HStack justifyContent={'center'} sx={{width: "100%"}}>
+                <HStack justifyContent={'center'} sx={{ width: "100%" }}>
                     <VStack gap={1} alignItems={'center'} sx={styles.completeTextBox}>
-                        <Typography variant="h4" sx={styles.completeText}>Your quick check is complete</Typography>
+                        <Typography variant="h4" sx={styles.completeText}>
+                            Your quick check is complete
+                        </Typography>
                     </VStack>
                 </HStack>
-                {userProfile &&
+                {userProfile && (
                     <>
-                        <h3>These data points are in your profile:</h3>
+                        <Typography variant="h5">These data points are in your profile:</Typography>
                         {buildUserProfile()}
-                    </>}
-                <ButtonCard link={`/eligibility-overview`} text="Discover your benefits"
-                            backgroundColor={globalStyles.secondaryColor}/>
+                    </>
+                )}
+                <ButtonCard
+                    link={`/eligibility-overview`}
+                    text="Discover your benefits"
+                    backgroundColor={globalStyles.secondaryColor}
+                />
             </VStack>
-        </VStack>);
+        </VStack>
+    );
 };
 
 const styles = {
