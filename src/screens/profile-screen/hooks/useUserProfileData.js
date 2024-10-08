93698af830a51282ba409e8776697c617b7b45ab
wbglaeser <ben.glaeser@tuta.io>
2024-10-08 21:11:29 +0200

    fix: pass translation function rdf conversion

--- src/screens/profile-screen/hooks/useUserProfileData.js
+++ src/screens/profile-screen/hooks/useUserProfileData.js
@@ -2,8 +2,10 @@ import {useMemo} from 'react';
 import {useMetadataStore} from "../../../storage/zustand";
 import {UserModel} from "../../../models/UserModel";
 import {convertUserValueRaw, expand} from "../../../utilities/rdfParsing";
+import useTranslation from "../../../language/useTranslation";
 
 const useUserProfileData = () => {
+    const { t } = useTranslation();
     const metadata = useMetadataStore((state) => state.metadata);
     const userProfile = UserModel.retrieveUserData("ff:quick-check-user");
 
@@ -13,13 +15,13 @@ const useUserProfileData = () => {
             .map(([key, value]) => {
                 const dfObj = metadata.df[expand(key)];
                 const dfLabel = dfObj.label;
-                const dfValue = convertUserValueRaw(value, dfObj);
+                const dfValue = convertUserValueRaw(value, dfObj, t);
                 return {
                     label: dfLabel,
                     value: dfValue
                 };
             });
-    }, [userProfile, metadata]);
+    }, [userProfile, metadata.df, t]);
 };
 
 export default useUserProfileData;
