036fb576c53955f50fc36621dabe89e8a983dc29
wbglaeser <ben.glaeser@tuta.io>
2024-11-07 09:30:07 +0100

    add disclaimer

--- src/screens/landing-page/sections/funding/components/LandingPageSupportCardNGI.js
+++ src/screens/landing-page/sections/funding/components/LandingPageSupportCardNGI.js
@@ -1,7 +1,11 @@
+import React from "react";
 import VStack from "../../../../../components/VStack";
 import HStack from "../../../../../components/HStack";
-import {Typography} from "@mui/material";
+import {IconButton, Typography} from "@mui/material";
 import useTranslation from "../../../../../language/useTranslation";
+import globalStyles from "../../../../../styles/styles";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 
 const Stacker = ({isDesktop, children}) => {
     return (
@@ -19,7 +23,9 @@ const Stacker = ({isDesktop, children}) => {
 }
 
 const LandingPageSupportCard = ({isDesktop}) => {
+    const [showDisclaimer, setShowDisclaimer] = React.useState(false);
     const {t} = useTranslation();
+
 
     const NGISearchLogoUrl = `${process.env.PUBLIC_URL}/assets/images/logos/NGI-Search-Logo.png`;
     const EuLogoUrl = `${process.env.PUBLIC_URL}/assets/images/logos/EU-Logo.png`;
@@ -36,16 +42,46 @@ const LandingPageSupportCard = ({isDesktop}) => {
                     </Typography>
                 </VStack>
                 <VStack justifyContent={'flex-end'} alignItems={'center'} sx={{width: "125px", height: "125px"}}>
-                    <img src={NGISearchLogoUrl} alt={'Prototype Fund Logo'} style={{maxWidth: "125px", maxHeight: "125px"}}/>
+                    <img src={NGISearchLogoUrl} alt={'Prototype Fund Logo'}
+                         style={{maxWidth: "125px", maxHeight: "125px"}}/>
                 </VStack>
                 <VStack justifyContent={'flex-end'} alignItems={'center'} sx={{width: "125px", height: "125px"}}>
                     <img src={EuLogoUrl} alt={'BMBF Logo'} style={{maxWidth: "125px", maxHeight: "125px"}}/>
                 </VStack>
             </Stacker>
-            <VStack>
-                <Typography sx={styles.disclaimerText}>
-                    {t('home.supportedBy.disclaimerNGI')}
-                </Typography>
+            <VStack gap={1}>
+                <HStack gap={1} alignItems={'center'}>
+                    <Typography sx={styles.disclaimerText}>
+                        Disclaimer
+                    </Typography>
+                    <IconButton
+                        onClick={() => setShowDisclaimer(!showDisclaimer)}
+                        sx={{
+                            width: 25,
+                            height: 25,
+                            borderRadius: '50%',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            backgroundColor: 'white',
+                            '&:hover': {
+                                backgroundColor: globalStyles.colorLightGrey,
+                            },
+                        }}
+                    >
+                        {
+                            showDisclaimer ?
+                                <ExpandLessIcon sx={{fontSize: 20, color: globalStyles.colorDarkGrey}}/> :
+                                <ExpandMoreIcon sx={{fontSize: 20, color: globalStyles.colorDarkGrey}}/>
+                        }
+                    </IconButton>
+                </HStack>
+                {
+                    showDisclaimer &&
+                    <Typography sx={styles.disclaimerText}>
+                        {t('home.supportedBy.disclaimerNGI')}
+                    </Typography>
+                }
             </VStack>
         </VStack>
     )
@@ -67,7 +103,7 @@ const styles = {
     },
     disclaimerText: {
         fontSize: '12px',
-        color: 'rgba(0, 0, 0, 0.5)',
+        color: globalStyles.colorDarkGrey,
     }
 };
 
