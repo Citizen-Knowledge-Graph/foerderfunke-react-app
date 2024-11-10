7de98f09e69720556b82d63e63a3566e3586a8d7
wbglaeser <ben.glaeser@tuta.io>
2024-11-10 17:35:01 +0100

    translate past milestones to german

--- src/screens/activity-log/sections/milestones/content/milestonesList.js
+++ src/screens/activity-log/sections/milestones/content/milestonesList.js
@@ -101,7 +101,7 @@ export const milestonesList = {
                 description: 'In July 2023, Helen Bielawa wrote an article about the concept in the Tagesspiegel Background, titled "Proaktive Verwaltung als Selfmade-Lösung".',
                 links: [{title: 'Article', url: 'https://background.tagesspiegel.de/smart-city-und-stadtentwicklung/briefing/proaktive-verwaltung-als-selfmade-loesung'}],
                 date: 'July 2023',
-            },
+            }
         ]
     },
     de: {
@@ -124,18 +124,93 @@ export const milestonesList = {
         ],
         past: [
             {
-                title: 'Launch der Webanwendung und Demo Day Prototype Fund',
-                description: 'Im Rahmen unserer Teilnahme am Demo Day des Prototype Funds haben wir den MVP unserer ' +
-                    'Webanwendung gelauncht. Die Anwendung enthält derzeit 10 Leistungen und steht allen in Deutschland ' +
-                    'lebenden Personen zur Verfügung.',
+                title: 'Erfolgreiche Bewerbung: NGI Search',
+                description: 'Wir haben uns erfolgreich für das NGI Search Programm der Europäischen Kommission beworben und werden bis Mitte 2025 Fördermittel, Mentoring und verschiedene Unterstützungsmodule erhalten.',
+                links: [{title: 'NGI Search', url: 'https://www.ngisearch.eu/'}],
+                date: 'November 2024'
+            },
+            {
+                title: 'Präsentation beim Public Makers Bootcamp',
+                description: 'Ben präsentierte FörderFunke und erzählte von seine Erfahrungen im öffentlichen Sektor auf dem ersten Public Makers Bootcamp in München.',
+                links: [{title: 'Public Makers Bootcamp', url: 'https://www.linkedin.com/feed/update/urn:li:activity:7253815779312238595/'}],
+                date: 'Oktober 2024'
+            },
+            {
+                title: 'Präsentation beim "The Tech We Want Online Summit"',
+                description: 'Benjamin präsentierte FörderFunke auf dem "The Tech We Want Online Summit", organisiert von der Open Knowledge Foundation.',
+                links: [{title: 'The Tech We Want Online Summit', url: 'https://www.youtube.com/live/V7Sxo0IXe8g?t=20005s'}],
+                date: 'Oktober 2024'
+            },
+            {
+                title: 'Workshop bei der NEGZ-Herbsttagung',
+                description: 'Auf der NEGZ-Herbsttagung haben wir einen Workshop zu den Ideen der FörderFunke veranstaltet.',
+                links: [{title: 'Workshop NEGZ-Herbsttagung', url: 'https://negz.org/termin/negz-herbsttagung-zur-digitalen-verwaltung-2024/#WS5'}],
+                date: 'September 2024'
+            },
+            {
+                title: 'Vorstellung des MVP beim Prototype Fund Demo Day',
+                description: 'Im Rahmen unserer Teilnahme am Demo Day des Prototype Fund haben wir den MVP der Webanwendung vorgestellt. Die Anwendung umfasst derzeit 10 Sozialleistungen in Deutschland.',
+                links: [{title: 'Prototype Fund Demo Day', url: 'https://prototypefund.de/demo-day/'}],
                 date: 'September 2024',
             },
             {
-                title: 'Beginn der Förderperiode des Prototype Funds',
-                description: 'Ende 2023 wurden wir für die 15. Runde des Prototype Funds ausgewählt. Die Förderperiode ' +
-                    'begann im März 2024 und dauerte 6 Monate. Die Förderung und zusätzliche Unterstützungsangebote ' +
-                    'ermöglichten es uns, die Anwendung von einer Idee zu einem funktionierenden Prototypen zu entwickeln.',
-                date: 'Anfang 2025',
+                title: 'Präsentation vor Bund-Länder-Arbeitsgruppe',
+                description: 'Unser Projekt wurde als "Good-Practice-Beispiel" in der Sitzung der Bund-Länder-Arbeitsgruppe "Bürgerfreundlicher Sozialstaat" (BMAS) vorgestellt.',
+                date: 'September 2024'
+            },
+            {
+                title: 'Civic Coding Projektberatung',
+                description: 'Wir wurden von Civic Coding sowohl in technischer als auch in organisatorischer Hinsicht beraten.',
+                links: [{title: 'Civic Coding', url: 'https://www.civic-coding.de/'}],
+                date: 'Mai - September 2024'
+            },
+            {
+                title: 'Finalist Innovationwettbewerb München',
+                description: 'Wir wurden als Finalisten für den Innovationswettbewerb der Stadt München ausgewählt.',
+                links: [{title: 'Munich Innovation Competition', url: 'https://stadt.muenchen.de/infos/innovationswettbewerb.html'}],
+                date: 'Juli 2024'
+            },
+            {
+                title: 'Teilnahme am Bertelsmann Datendialog',
+                description: 'Wir haben an der Veranstaltung Datendialog teilgenommen, die von der Bertelsmann-Stiftung und CorrelAid ausgerichtet wurde.',
+                links: [{title: 'Datendialog', url: 'https://www.bertelsmann-stiftung.de/de/unsere-projekte/data-science/projektnachrichten/datendialog-hamburg-juni-2024'}],
+                date: 'Juni 2024'
+            },
+            {
+                title: 'Präsentation bei der Digitalen Woche Kiel',
+                description: 'Wir haben unser Projekt auf der Digitalen Woche Kiel vorgestellt und uns dabei auf Solid als Datenspeicherlösung konzentriert, die wir gerne einsetzen würden.',
+                links: [{title: 'Präsentation Digitale Woche Kiel', url: 'https://digitalewochekiel.de/'}],
+                date: 'Mai 2024'
+            },
+            {
+                title: 'Präsentation bei der Arbeitsgruppe Semantische Interoperabilität',
+                description: 'Wir wurden eingeladen, FörderFunke als Anwendungsfall für Linked Open Data in der Arbeitsgruppe "Semantische Interoperabilität" des IT-Planungsrates vorzustellen.',
+                links: [{title: 'IT-Planungsrat', url: 'https://www.it-planungsrat.de/'}],
+                date: 'April 2024'
+            },
+            {
+                title: 'Start der Prototype Fund Förderperiode',
+                description: 'Ende 2023 wurden wir für die Teilnahme an der 15. Runde des Prototypenfonds ausgewählt. Der Förderzeitraum begann im März 2024 und dauerte 6 Monate. Die Finanzierung und die zusätzlichen Unterstützungsprogramme boten uns die Gelegenheit, die Anwendung von einer Idee zu einem funktionierenden Prototyp zu entwickeln.',
+                links: [{title: 'Prototype Fund', url: 'https://prototypefund.de/project/foerderfunke/'}],
+                date: 'März 2024',
+            },
+            {
+                title: 'Video für das Creative Bureaucracy Festival',
+                description: 'Für den Digital Kick-Off Day des Creative Bureaucracy Festivals haben wir ein 7-minütiges Video produziert.',
+                links: [{title: 'Video Beitrag', url: 'https://youtu.be/fDWfGEXvqjk'}],
+                date: 'März 2024',
+            },
+            {
+                title: 'Best Poster Award',
+                description: 'Benjamin erhielt den Preis für das beste Poster im Online-Kurs "Linked Data & Solid" an der Universität Gent.',
+                links: [{title: 'Poster Award', url: 'https://www.linkedin.com/posts/bdegenhart_linkeddata-semanticweb-solid-activity-7168706749070262275-S5Qu/'}],
+                date: 'Januar 2024',
+            },
+            {
+                title: 'Artikel im Tagesspiegel Background',
+                description: 'Im Juli 2023 schrieb Helen Bielawa einen Artikel über das Konzept im Tagesspiegel Background mit dem Titel "Proaktive Verwaltung als Selfmade-Lösung".',
+                links: [{title: 'Artikel', url: 'https://background.tagesspiegel.de/smart-city-und-stadtentwicklung/briefing/proaktive-verwaltung-als-selfmade-loesung'}],
+                date: 'Juli 2023',
             }
         ]
     }
