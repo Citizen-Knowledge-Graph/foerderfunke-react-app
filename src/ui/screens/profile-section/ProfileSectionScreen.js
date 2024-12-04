17d1c6a3d9f1f8b8b3a156c27d00cceba8066df0
wbglaeser <ben.glaeser@tuta.io>
2024-12-04 12:27:11 -0300

    delete ununsed component

--- src/ui/screens/profile-section/ProfileSectionScreen.js
+++ src/ui/screens/profile-section/ProfileSectionScreen.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import Layout from "../../shared-components/Layout";
-import ProfileSectionList from "./components/ProfileSectionList";
 import ProfileSectionCompleted from "./components/ProfileSectionCompleted";
 import AppScreenWrapper from "../../shared-components/AppScreenWrapper";
 import {useStore} from "../../shared-components/ViewportUpdater";
@@ -11,23 +10,15 @@ const ProfileSectionScreen = () => {
     const isDesktop = useStore((state) => state.isDesktop);
     const profileQuestions = useQuestionsStore((state) => state.questions);
     const [completed, setCompleted] = useState(false);
-    const fixedSetOfQuestionsMode = false;
 
     return (
         <Layout isApp={true} logo={false}>
             <AppScreenWrapper isDesktop={isDesktop} data-testid={"app wrapper"}>
                 {profileQuestions ? (
                     !completed ? (
-                        fixedSetOfQuestionsMode ? (
-                                <ProfileSectionList
-                                    profileQuestions={profileQuestions}
-                                    setCompleted={setCompleted}
-                                />
-                            ) : (
-                                <ProfileSectionTopQuestion
-                                    setCompleted={setCompleted}
-                                 />
-                            )
+                        <ProfileSectionTopQuestion
+                            setCompleted={setCompleted}
+                        />
                     ) : (
                         <ProfileSectionCompleted/>
                     )
