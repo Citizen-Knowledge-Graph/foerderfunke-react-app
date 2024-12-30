3dee219b79a4b1f0b7795f24741e1476ad597d4e
ben.glaeser <ben.glaeser@tuta.io>
2024-12-30 20:15:47 +0100

    move benefit rules page to grid sytem

--- src/ui/screens/benefit-page/components/BenefitPageRuleEntry.js
+++ src/ui/screens/benefit-page/components/BenefitPageRuleEntry.js
@@ -1,80 +1,54 @@
 import React from "react";
-import globalStyles from "../../../styles/styles";
-import VStack from "../../../shared-components/VStack";
-import {Typography} from "@mui/material";
-import HStack from "../../../shared-components/HStack";
+import { Grid, Typography } from "@mui/material";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import HelpIcon from '@mui/icons-material/Help';
 import CancelIcon from '@mui/icons-material/Cancel';
-import {useStore} from "../../../shared-components/ViewportUpdater";
 import useTranslation from "../../../language/useTranslation";
+import globalStyles from "../../../styles/styles";
 
-const BenefitPageRuleEntry = ({ruleData}) => {
+const BenefitPageRuleEntry = ({ ruleData }) => {
     const { t } = useTranslation();
-    const isDesktop = useStore((state) => state.isDesktop);
 
     const icon = (() => {
         switch (ruleData.validity) {
             case 'valid':
-                return <CheckCircleIcon sx={{fontSize: 30, color: globalStyles.secondaryColor}}/>;
+                return <CheckCircleIcon sx={{ fontSize: 30, color: globalStyles.secondaryColor }} />;
             case 'missing':
-                return <HelpIcon sx={{fontSize: 30, color: globalStyles.colorDarkGrey}}/>;
+                return <HelpIcon sx={{ fontSize: 30, color: globalStyles.colorDarkGrey }} />;
             case 'invalid':
-                return <CancelIcon sx={{fontSize: 30, color: globalStyles.colorCoralRed}}/>;
+                return <CancelIcon sx={{ fontSize: 30, color: globalStyles.colorCoralRed }} />;
             default:
-                return <HelpIcon sx={{fontSize: 30, color: globalStyles.colorDarkGrey}}/>;
+                return <HelpIcon sx={{ fontSize: 30, color: globalStyles.colorDarkGrey }} />;
         }
     })();
 
-    const DynamicStacker = ({children}) => {
-        if (isDesktop) {
-            return (
-                <HStack gap={3} alignItems={'center'}
-                        sx={{
-                            padding: '12px',
-                            borderRadius: '12px',
-                        }}>
-                    {children}
-                </HStack>
-            )
-        }
-        return (
-            <VStack
-                gap={1}
-                sx={{
-                    padding: '12px',
-                    borderRadius: '12px'
-            }}>
-                {children}
-            </VStack>
-        )
-    }
-
     return (
-        <DynamicStacker>
-            <HStack sx={{width: '100%'}}>
-                <VStack gap={0} sx={{width: '50%'}}>
-                    <Typography variant="body2">
-                        {ruleData.requirement.label}
-                    </Typography>
-                    <Typography variant="body1">
-                        {ruleData.requirement.rule}
-                    </Typography>
-                </VStack>
-                <VStack gap={0} sx={{width: '50%'}}>
-                    <Typography variant="body2">
-                        {t('app.benefitPage.rulesTable.yourAnswer')}
-                    </Typography>
-                    <Typography variant="body1">
-                        {ruleData.userValue}
-                    </Typography>
-                </VStack>
-            </HStack>
-            <HStack justifyContent={'flex-end'}>
+        <Grid
+            container
+            spacing={2}
+            sx={{
+                padding: '12px',
+                borderRadius: '12px',
+                alignItems: 'center',
+            }}
+        >
+            <Grid item xs={6} sm={5}>
+                <Typography variant="body2">{ruleData.requirement.label}</Typography>
+                <Typography variant="body1">{ruleData.requirement.rule}</Typography>
+            </Grid>
+            <Grid item xs={6} sm={5}>
+                <Typography variant="body2">
+                    {t('app.benefitPage.rulesTable.yourAnswer')}
+                </Typography>
+                <Typography variant="body1">{ruleData.userValue}</Typography>
+            </Grid>
+            <Grid item xs={12} sm={2}
+                sx={{ display: 'flex', justifyContent: "flex-end"}}
+            >
                 {icon}
-            </HStack>
-        </DynamicStacker>
-    )
-}
+            </Grid>
+        </Grid>
+    );
+};
 
 export default BenefitPageRuleEntry;
