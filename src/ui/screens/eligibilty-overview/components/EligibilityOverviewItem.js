dcb0f2bb811bd6615ff79bf4a4bf77e7944b464c
ben.glaeser <ben.glaeser@tuta.io>
2024-12-31 14:37:02 +0100

    update EligibilityOverviewItem

--- src/ui/screens/eligibilty-overview/components/EligibilityOverviewItem.js
+++ src/ui/screens/eligibilty-overview/components/EligibilityOverviewItem.js
@@ -1,12 +1,11 @@
-import React, {useState} from 'react';
-import {IconButton, Typography} from '@mui/material';
-import VStack from "../../../shared-components/VStack";
-import HStack from "../../../shared-components/HStack";
-import {green, red, grey} from "@mui/material/colors";
-import {Circle, Add, Remove} from "@mui/icons-material";
+import React, { useState } from 'react';
+import { IconButton, Typography, Box } from '@mui/material';
+import { HBox, VBox } from '../../../shared-components/LayoutBoxes';
+import { green, red, grey } from "@mui/material/colors";
+import { Circle, Add, Remove } from "@mui/icons-material";
 import EligibilityOverviewItemDetails from "./EligibilityOverviewItemDetails";
 
-const EligibilityOverviewItem = ({item, eligible}) => {
+const EligibilityOverviewItem = ({ item, eligible }) => {
     const [showDescription, setShowDescription] = useState(false);
 
     const toggleDescription = () => {
@@ -15,29 +14,28 @@ const EligibilityOverviewItem = ({item, eligible}) => {
 
     const bottomMargin = showDescription ? '16px' : '0';
     const fontWeight = showDescription ? 'bold' : '400';
-
     const color = (eligible === 'eligible') ? green[500] : ((eligible === 'non-eligible') ? red[500] :
         grey[500]);
 
     return (
-        <HStack justifyContent={'flex-start'} sx={{width: '100%', marginBottom: bottomMargin}}>
-            <Circle sx={{color: color}}/>
-            <HStack justifyContent={'space-between'} sx={{width: '100%'}}>
-                <VStack gap={1} alignItems={'flex-start'}>
-                    <Typography variant='h6' sx={{fontWeight: fontWeight}} onClick={toggleDescription}>
+        <HBox sx={{ width: '100%', marginBottom: bottomMargin }}>
+            <Circle sx={{ color: color }} />
+            <HBox sx={{ width: '100%' }}>
+                <VBox>
+                    <Typography variant='h6' sx={{ fontWeight: fontWeight }} onClick={toggleDescription}>
                         {item.title}
                     </Typography>
                     {showDescription && (
-                        <EligibilityOverviewItemDetails item={item} eligible={eligible}/>
+                        <EligibilityOverviewItemDetails item={item} eligible={eligible} />
                     )}
-                </VStack>
-            </HStack>
-            <VStack gap={0} alignItems={'flex-start'}>
+                </VBox>
+            </HBox>
+            <Box>
                 <IconButton onClick={toggleDescription} size="small" >
-                    {showDescription ? <Remove sx={{color: 'black'}}/> : <Add sx={{color: 'black'}}/>}
+                    {showDescription ? <Remove sx={{ color: 'black' }} /> : <Add sx={{ color: 'black' }} />}
                 </IconButton>
-            </VStack>
-        </HStack>
+            </Box>
+        </HBox>
     );
 };
 
