81f35ed16ecc9443bb3cc5eb0b26c9dd9db50e07
ben.glaeser <ben.glaeser@tuta.io>
2024-12-31 11:53:09 +0100

    fix eligibility header

--- src/ui/screens/eligibilty-overview/components/EligibilityOverviewHeader.js
+++ src/ui/screens/eligibilty-overview/components/EligibilityOverviewHeader.js
@@ -1,87 +1,58 @@
 import React from 'react';
-import {IconButton, Typography} from '@mui/material';
-import {Link} from "react-router-dom";
-import globalStyles from "../../../styles/styles";
+import { Link } from "react-router-dom";
+import { IconButton, Typography } from '@mui/material';
 import InfoIcon from "@mui/icons-material/Info";
 import PersonIcon from '@mui/icons-material/Person';
-import VStack from "../../../shared-components/VStack";
-import HStack from "../../../shared-components/HStack";
+import globalStyles from "../../../styles/styles";
+import ContentBox from '../../../shared-components/ContentBox';
+import { VBox, HBox } from "../../../shared-components/LayoutBoxes";
 import useTranslation from "../../../language/useTranslation";
+import theme from "../../../../theme";
 
-const EligibilityOverviewScreen = ({isDesktop}) => {
+const EligibilityOverviewScreen = ({ isDesktop }) => {
     const { t } = useTranslation();
-    const titleFontSize = isDesktop ? '32px' : '28px';
 
     return (
-        <VStack>
-            <VStack gap={0} alignItems={'flex-start'} sx={{
-                width: '100%',
-                backgroundColor: globalStyles.primaryColor,
-                padding: '16px',
-                borderRadius: '12px'
-            }}>
-                <VStack alignItems={'flex-start'} gap={3} sx={{width: '100%'}}>
-                    <HStack justifyContent={'space-between'} sx={{width: '100%'}}>
-                        <VStack gap={2} sx={{width: '100%'}}>
-                            <HStack justifyContent={'space-between'}>
-                            <Typography sx={{...styles.titleText, fontSize: titleFontSize}}>
-                                {t('app.browseAll.header')}
-                            </Typography>
-                                <IconButton
-                                    component={Link}
-                                    to='/profile-overview'
-                                    sx={{
-                                        width: 40,
-                                        height: 40,
-                                        borderRadius: '50%',
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        justifyContent: 'center',
-                                        backgroundColor: 'white',
-                                        '&:hover': {
-                                            backgroundColor: globalStyles.colorLightGrey,
-                                        },
-                                    }}
-                                >
-                                    <PersonIcon sx={{ fontSize: 20, color: 'black' }} />
-                                </IconButton>
-                            </HStack>
-                            <Typography sx={styles.subTitleCardText}>
-                                {t('app.browseAll.subtitle')}
-                            </Typography>
-                        </VStack>
-                    </HStack>
-                </VStack>
-            </VStack>
-            <VStack sx={{width: '100%'}}>
-                <HStack sx={styles.liableInfoBox} justifyContent={'flex-start'} alignItems={'center'}>
-                    <InfoIcon sx={{fontSize: '16px'}}/>
-                    <Typography sx={styles.liableInfoText}>
+        <ContentBox sx={{
+            width: "100%",
+            backgroundColor: theme.palette.primary.main,
+        }}>
+            <VBox sx={{ alignItems: "flex-start", gap: theme.spacing(2) }}>
+                <HBox sx={{ width: "100%", justifyContent: 'space-between' }}>
+                    <Typography variant="h4">
+                        {t('app.browseAll.header')}
+                    </Typography>
+                    <IconButton
+                        component={Link}
+                        to='/profile-overview'
+                        sx={{
+                            width: 40,
+                            height: 40,
+                            borderRadius: '50%',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            backgroundColor: 'white',
+                            '&:hover': {
+                                backgroundColor: globalStyles.colorLightGrey,
+                            },
+                        }}
+                    >
+                        <PersonIcon sx={{ fontSize: 20, color: 'black' }} />
+                    </IconButton>
+                </HBox>
+                <Typography variant='body1'>
+                    {t('app.browseAll.subtitle')}
+                </Typography>
+                <HBox>
+                    <InfoIcon sx={{ fontSize: '16px' }} />
+                    <Typography variant='body2'>
                         {t('app.browseAll.info')}
                     </Typography>
-                < /HStack>
-            </VStack>
-        </VStack>
+                </HBox>
+            </VBox>
+        </ContentBox >
     );
-};
-
-const styles = {
-    titleText: {
-        fontWeight: 'bold',
-    },
-    subTitleCardText: {
-        fontSize: '16px',
-        fontWeight: '400',
-        color: 'black',
-        textTransform: 'none',
-    },
-    liableInfoBox: {
-        padding: '12px',
-        borderRadius: '12px'
-    },
-    liableInfoText: {
-        fontSize: '14px'
-    }
 };
 
 export default EligibilityOverviewScreen;
