22e3a49640e35fae846a50ec097de3cd26daeabf
ben.glaeser <ben.glaeser@tuta.io>
2025-02-25 11:15:21 +0100

    add button to fact section in landing paghe

--- src/ui/screens/landing-page/sections/fact/LandingPageFact.js
+++ src/ui/screens/landing-page/sections/fact/LandingPageFact.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 import LandingPageSectionWrapper from "../../components/LandingPageSectionWrapper";
 import useTranslation from "../../../../language/useTranslation";
 import { HBox, VBox } from "../../../../shared-components/LayoutBoxes";
@@ -15,26 +16,42 @@ const LandingPageFact = ({ isDesktop }) => {
                 paddingBottom: '104px',
                 backgroundColor: `${theme.palette.white.dark}40`,
                 borderRadius: theme.shape.borderRadius,
-                alignItems: 'center',
             }}>
-                <VBox
-                    sx={{
-                        maxWidth: '620px',
-                        alignItems: 'center',
-                        gap: theme.spacing(4),
-                    }}
-                >
-                    <VBox sx={{alignItems: 'center'}}>
-                        <Typography variant="h1" sx={{color: theme.palette.pink.main}}>15.000.000.000 Euro</Typography>
-                        <Typography variant="h4" sx={{
-                            color: theme.palette.black.main, 
-                            textAlign: 'center', 
-                            fontWeight: '400',
-                            maxWidth: '400px',
-                        }}>{t('home.mission.subHeader')}</Typography>
+                <VBox sx={{
+                    gap: theme.spacing(6), alignItems: 'center',
+                }}>
+                    <VBox
+                        sx={{
+                            maxWidth: '620px',
+                            alignItems: 'center',
+                            gap: theme.spacing(4),
+                        }}
+                    >
+                        <VBox sx={{ alignItems: 'center' }}>
+                            <Typography variant="h1" sx={{ color: theme.palette.pink.main }}>15.000.000.000 Euro</Typography>
+                            <Typography variant="h4" sx={{
+                                color: theme.palette.black.main,
+                                textAlign: 'center',
+                                fontWeight: '400',
+                                maxWidth: '400px',
+                            }}>{t('home.mission.subHeader')}</Typography>
+                        </VBox>
+                        <HBox>
+                            <Typography variant="body1" sx={{ textAlign: 'center' }} >{t('home.mission.text')}</Typography>
+                        </HBox>
                     </VBox>
-                    <HBox>
-                        <Typography variant="body1" sx={{textAlign: 'center'}} >{t('home.mission.text')}</Typography>
+                    <HBox sx={{ gap: theme.spacing(2) }}>
+                        <Button variant="contained"
+                            sx={{ 
+                                '&:hover': {
+                                    backgroundColor: theme.palette.yellow.main,
+                                    color: theme.palette.black.main,
+                                    borderColor: theme.palette.yellow.main
+                                }
+                            }}
+                            component={Link}
+                            to="/user-routing">{t('home.global.actionButton')}
+                        </Button>
                     </HBox>
                 </VBox>
             </VBox>
--- src/ui/screens/landing-page/sections/how-it-works/LandingPageHowItWorks.js
+++ src/ui/screens/landing-page/sections/how-it-works/LandingPageHowItWorks.js
@@ -3,10 +3,9 @@ import VStack from "../../../../shared-components/VStack";
 import LandingPageHowItWorksDesktop from "./views/LandingPageHowItWorksDesktop";
 import LandingPageHowItWorksMobile from "./views/LandingPageHowItWorksMobile";
 import LandingPageSectionWrapper from "../../components/LandingPageSectionWrapper";
-import LandingPageSectionGrid from "../../components/LandingPageSectionGrid";
 import useTranslation from "../../../../language/useTranslation";
 
-const LandingPageHowItWorks = ({isDesktop}) => {
+const LandingPageHowItWorks = ({ isDesktop }) => {
     const { t } = useTranslation();
 
     const quick_check = `${process.env.PUBLIC_URL}/assets/images/landing-page/current_quickcheck_page.jpg`;
@@ -15,16 +14,12 @@ const LandingPageHowItWorks = ({isDesktop}) => {
 
     return (
         <LandingPageSectionWrapper isDesktop={isDesktop}>
-            <LandingPageSectionGrid title={t('home.howItWorks.header')}>
-                <VStack gap={9} alignItems={'center'}>
-                    {isDesktop ?
-                        <LandingPageHowItWorksDesktop quick_check={quick_check} benefits_overview={benefits_overview}
-                                                      benefit_page={benefit_page}/>
-                        : <LandingPageHowItWorksMobile quick_check={quick_check} benefits_overview={benefits_overview}
-                                                       benefit_page={benefit_page}/>
-                    }
-                </VStack>
-            </LandingPageSectionGrid>
+            {isDesktop ?
+                <LandingPageHowItWorksDesktop quick_check={quick_check} benefits_overview={benefits_overview}
+                    benefit_page={benefit_page} />
+                : <LandingPageHowItWorksMobile quick_check={quick_check} benefits_overview={benefits_overview}
+                    benefit_page={benefit_page} />
+            }
         </LandingPageSectionWrapper>
     );
 }
