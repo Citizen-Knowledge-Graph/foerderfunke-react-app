07030f6eedc5f02aaad285913ad4a8b8887a6082
ben.glaeser <ben.glaeser@tuta.io>
2025-02-25 13:41:48 +0100

    add new section - thats how it works

--- src/ui/screens/landing-page/sections/principles/LandingPagePrinciples.js
+++ src/ui/screens/landing-page/sections/principles/LandingPagePrinciples.js
@@ -1,26 +1,53 @@
 import React from "react";
 import LandingPageSectionWrapper from "../../components/LandingPageSectionWrapper";
-import LandingPageSectionGrid from "../../components/LandingPageSectionGrid";
-import VStack from "../../../../shared-components/VStack";
-import LandingPagePrincipleCardData from "./components/LandingPagePrincipleCardData";
-import LandingPagePrincipleCardPrivacy from "./components/LandingPagePrincipleCardPrivacy";
-import LandingPagePrincipleCardUser from "./components/LandingPagePrincipleCardUser";
 import useTranslation from "../../../../language/useTranslation";
+import theme from "../../../../../theme";
+import { HBox, VBox } from "../../../../shared-components/LayoutBoxes";
+import { Typography } from "@mui/material";
 
-const LandingPagePrinciples = ({isDesktop}) => {
+const LandingPagePrinciples = ({ isDesktop }) => {
     const { t } = useTranslation();
+    const locket = `${process.env.PUBLIC_URL}/assets/images/landing-page/lockets.svg`;
 
     return (
         <LandingPageSectionWrapper isDesktop={isDesktop}>
-            <LandingPageSectionGrid title={t('home.principles.header')}>
-                <VStack gap={5}>
-                    <VStack gap={7} justifyContent={'center'} sx={{width: '100%'}}>
-                        <LandingPagePrincipleCardData isDesktop={isDesktop} gifFirst={false}/>
-                        <LandingPagePrincipleCardPrivacy isDesktop={isDesktop}/>
-                        <LandingPagePrincipleCardUser isDesktop={isDesktop}/>
-                    </VStack>
-                </VStack>
-            </LandingPageSectionGrid>
+            <VBox sx={{
+                paddingTop: '104px',
+                paddingBottom: '104px',
+                paddingLeft: '96px',
+                paddingRight: '96px',
+                alignItems: 'center',
+            }}>
+                <HBox sx={{ gap: theme.spacing(8) }}>
+                    <VBox sx={{ maxWidth: '760px', gap: theme.spacing(6) }}>
+                        <Typography variant="h1" sx={{ color: theme.palette.black.main }}>
+                            Und so funktioniert's
+                        </Typography>
+                        <VBox>
+                            <Typography variant="h4" sx={{ fontWeight: 400, color: theme.palette.black.main }}>
+                                Privacy by Design
+                            </Typography>
+                            <Typography variant="body1" sx={{ color: theme.palette.black.main }}>
+                                {t('home.principles.part1Text')}
+                            </Typography>
+                        </VBox>
+                        <VBox>
+                            <Typography variant="h4" sx={{ fontWeight: 400, color: theme.palette.black.main }}>
+                                Privacy by Design
+                            </Typography>
+                            <Typography variant="body1" sx={{ color: theme.palette.black.main }}>
+                                {t('home.principles.part2Text')}
+                            </Typography>
+                        </VBox>
+                    </VBox>
+                    <VBox>
+                        <img
+                            src={locket}
+                            alt="logo"
+                        />
+                    </VBox>
+                </HBox>
+            </VBox>
         </LandingPageSectionWrapper>
     );
 }
