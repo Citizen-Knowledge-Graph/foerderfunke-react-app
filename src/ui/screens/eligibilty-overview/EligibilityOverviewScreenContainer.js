c3d81138c53d4206d2896fcb362fddde8b32f1c2
ben.glaeser <ben.glaeser@tuta.io>
2025-07-07 20:29:03 +0100

    allow to call catalog with filter view

--- src/ui/screens/eligibilty-overview/EligibilityOverviewScreenContainer.js
+++ src/ui/screens/eligibilty-overview/EligibilityOverviewScreenContainer.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import useFetchData from "@/ui/shared-hooks/utility/useFetchResource";
 import useTranslation from "@/ui/language/useTranslation";
 import EligibilityOverviewScreen from './EligibilityOverviewScreen';
@@ -11,13 +12,38 @@ import useFilterEligibilityData from './hooks/useFilterEligibilityData';
 const EligibilityOverviewScreenContainer = () => {
     const { t } = useTranslation();
     const language = useLanguageStore((state) => state.language);
-    const [filters, setFilters] = useState({});
 
     const hydrationData = useFetchData('assets/data/requirement-profiles/requirement-profiles-hydration.json');
     const { validationReport } = useProduceValidationReport();
     const metadata = useMetadataStore((state) => state.metadata);
     const { eligibilityData, filterOptions } = useEligibilityData(validationReport, metadata, hydrationData, language);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const filters = useMemo(() => {
+        const f = {};
+        for (let key of Object.keys(filterOptions)) {
+            const allForKey = searchParams.getAll(key);
+            if (allForKey.length) {
+                f[key] = allForKey;
+            }
+        }
+        return f;
+    }, [searchParams, filterOptions]);
+
+    const handleChangeFilters = (updater) => {
+        const newFilters = updater(filters);
+        const next = new URLSearchParams();
+        for (let [key, vals] of Object.entries(newFilters)) {
+            ; (vals || []).forEach((v) => next.append(key, v));
+        }
+
+        setSearchParams(next, { replace: true });
+    };
+
     const filteredEligibilityData = useFilterEligibilityData(eligibilityData, filters);
+
+    console.log("print search parameters", searchParams.getAll('associatedLaws'));
 
     return (
         <EligibilityOverviewScreen
@@ -25,7 +51,7 @@ const EligibilityOverviewScreenContainer = () => {
             eligibilityData={filteredEligibilityData}
             filterOptions={filterOptions}
             filters={filters}
-            onChangeFilters={setFilters}
+            onChangeFilters={handleChangeFilters}
         />
     );
 };
