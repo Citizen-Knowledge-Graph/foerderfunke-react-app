ed7e9d8eff48b6487848d8696a8ea7736b833ec7
ben.glaeser <ben.glaeser@tuta.io>
2025-09-01 10:04:24 +0200

    add restart button to eli overview

--- src/ui/screens/eligibilty-overview/components/EligibilityOverviewHeader.js
+++ src/ui/screens/eligibilty-overview/components/EligibilityOverviewHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography } from '@mui/material';
-import { VBox } from "@/ui/shared-components/LayoutBoxes";
+import { HBox, VBox } from "@/ui/shared-components/LayoutBoxes";
 import useTranslation from "@/ui/language/useTranslation";
 import RegularButton from '@/ui/shared-components/buttons/RegularButton';
 
@@ -13,19 +13,27 @@ const EligibilityOverviewHeader = () => {
                 <Typography variant="h1">
                     {t('app.browseAll.header')}
                 </Typography>
-                <RegularButton
+                <HBox sx={{ alignItems: 'center' }}>
+                    <RegularButton
                         variant={'blueHollow'}
                         text={'app.browseAll.profileBtn'}
                         link={'/profile-overview'}
                         size='small'
                     />
+                    <RegularButton
+                        variant='greyContained'
+                        text={'app.qsComplete.restartBtn'}
+                        link={'/user-routing'}
+                        size='small'
+                    />
+                </HBox>
             </VBox>
             <VBox sx={{ maxWidth: '800px' }}>
                 <Typography variant='h2'>
                     {t('app.browseAll.disclaimerTitle')}
                 </Typography>
                 <Typography variant='body1'>
-                {t('app.browseAll.disclaimerText')}
+                    {t('app.browseAll.disclaimerText')}
                 </Typography>
             </VBox>
         </VBox>
