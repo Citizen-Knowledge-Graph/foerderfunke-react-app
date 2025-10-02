0f1c4cc0bd6a81d1b61e127756bb4295305bbaa4
ben.glaeser <ben.glaeser@tuta.io>
2025-10-02 12:02:15 +0200

    add bafoeg

--- src/ui/screens/benefit-page/components/BenefitPageExample.js
+++ src/ui/screens/benefit-page/components/BenefitPageExample.js
@@ -17,6 +17,7 @@ const BenefitPageExample = ({ idx, content }) => {
                 boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.25)',
                 padding: 2,
                 gap: 2,
+                flex: 1,
             }}
         >
             <VBox sx={{ gap: 2 }}>
